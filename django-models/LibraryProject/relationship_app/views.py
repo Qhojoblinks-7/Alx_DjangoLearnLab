ac33b0c3545e04ebd5c40abe3dd58f3b14b65359
Immanuel Quansah Eshun <124244985+Qhojoblinks-7@users.noreply.github.com>
2025-09-01 05:07:30 +0000

    push

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import permission_required, user_passes_test
 from django.views.generic import DetailView, CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.detail import DetailView
+
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth import login
