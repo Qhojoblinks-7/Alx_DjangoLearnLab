4a9019df6363d148fc2837a5e0837d189ab915a7
Immanuel Quansah Eshun <124244985+Qhojoblinks-7@users.noreply.github.com>
2025-09-10 13:14:41 +0000

    Fix CustomUser registration and clean up model duplication

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Book, CustomUser
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['email', 'first_name', 'last_name', 'is_staff', 'date_of_birth']
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
-    )
+from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,4 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -13,9 +45,8 @@ class Book(models.Model):
 
     class Meta:
         permissions = [
-            ("can_view_book", "Can view book"),
-            ("can_create_book", "Can create book"),
-            ("can_edit_book", "Can edit book"),
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
             ("can_delete_book", "Can delete book"),
         ]
 
