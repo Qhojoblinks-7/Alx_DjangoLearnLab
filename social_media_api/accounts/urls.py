4a383fcdc0366b56da44341b765769c2547c6399
Immanuel Quansah Eshun <124244985+Qhojoblinks-7@users.noreply.github.com>
2025-10-06 13:41:26 +0000

    feat(accounts): add follow and unfollow user endpoints

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import RegistrationView, LoginView, UserProfileViewSet
+from .views import RegistrationView, LoginView, UserProfileViewSet, FollowUserView, UnfollowUserView
 from django.urls import path, include
 
 
@@ -10,5 +10,7 @@ router.register(r'users', UserProfileViewSet, basename='user-profile')
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),  # ✅ follow/<int:user_id>/
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),  # ✅ unfollow/<int:user_id>/
     path('', include(router.urls)), # Includes /users/{pk}/ and /users/{pk}/follow/
 ]
